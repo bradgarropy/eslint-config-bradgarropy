c55bf14b65851a4cc8740f10241246eff8e5eb5d
Brad Garropy <bradgarropy@gmail.com>
2019-12-25 22:58:44 -0600

    update tests.

--- tests/app.js
+++ tests/app.js
@@ -1,5 +1,7 @@
-const hello = function() {
+const hello = async () => {
     console.log(
         "Hi, how are you? It's a great day to be alive! What is your name? Can we be best friends?",
     )
 }
+
+hello()
